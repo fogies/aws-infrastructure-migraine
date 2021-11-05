4682899eb353485e1ee83f0e648ced0668a3b21f
Anant Mittal <a42@Anants-MacBook-Pro.local>
2021-11-05 12:53:22 -0700

    Removed database initialization method. Don't need it in flask.

--- server_flask/database.py
+++ server_flask/database.py
@@ -27,49 +27,6 @@ class CouchDBClientConfig:
             admin_password=admin_password,
         )
 
-    @staticmethod
-    def initialize(self):
-        """
-        Initialize the database. Picked this method from tasks.database. Not being used in flask at the moment. Might want to move to aws-infrastructure code at some point. 
-        """
-        #
-        # Obtain our connection information and admin credentials
-        #
-        admin_auth = requests.auth.HTTPBasicAuth(
-            username=self._admin_user,
-            password=self._admin_password
-        )
-
-        #
-        # Ensure the database is initialized
-        #
-        try:
-            # Confirm the database is online
-            response = requests.get(
-                urljoin(self._baseurl, ''),
-            )
-            response.raise_for_status()
-
-            # Check whether the cluster has previously been finished (i.e., in a previous initialize)
-            response = requests.get(
-                urljoin(self._baseurl, '_cluster_setup'),
-                auth=admin_auth,
-            )
-            response.raise_for_status()
-
-            # If the cluster was not finished in a previous initialize, do that now
-            if response.json()['state'] != 'cluster_finished':
-                response = requests.post(
-                    urljoin(self._baseurl, '_cluster_setup'),
-                    json={
-                        'action': 'finish_cluster'
-                    },
-                    auth=admin_auth,
-                )
-                response.raise_for_status()
-        except requests.exceptions.HTTPError as error:
-            print(error)
-
     @property
     def admin_password(self) -> str:
         return self._admin_password
