d9ee83feb354aa7ea20762b66b38190466865873
James Fogarty <jayfo@users.noreply.github.com>
2021-11-08 14:57:23 -0800

    Refactor Config to Use Secret
    Nice thing about Python objects as configurations is you can do more than just put strings in there. And we already have database information in our secrets, so DRY.

--- server_flask/config/dev.py
+++ server_flask/config/dev.py
@@ -1,4 +1,22 @@
-class Config:
-    SECRET_KEY = 'DEVELOPMENT'
+import ruamel.yaml
 
-    URI_DATABASE = ''
+
+# Path is relative to server_flask
+COUCHDB_CLIENT_CONFIG_PATH = '../secrets/client/couchdb_client_config.yaml'
+
+
+class Config:
+    # TODO James: In production SECRET_KEY needs to be consistent across instances
+    SECRET_KEY: str = 'DEVELOPMENT'
+
+    URI_DATABASE: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    def __init__(self):
+        with open(COUCHDB_CLIENT_CONFIG_PATH) as file_couchdb_client_config:
+            couchdb_client_config = ruamel.yaml.safe_load(file_couchdb_client_config)
+
+        self.URI_DATABASE = couchdb_client_config['baseurl']
+        self.DB_USER = couchdb_client_config['admin']['user']
+        self.DB_PASSWORD = couchdb_client_config['admin']['password']
