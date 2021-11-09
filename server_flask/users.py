fc2cf82738e30363d01e3de8e592cc56415af1fc
James Fogarty <jayfo@users.noreply.github.com>
2021-11-08 18:03:06 -0800

    In Progress - De-indent

--- server_flask/users.py
+++ server_flask/users.py
@@ -94,104 +94,104 @@ def create_user_accounts():
 
     #### TODO
 
-        #
-        # Connect to the database
-        #
-        admin_session = _create_admin_session()
+    #
+    # Connect to the database
+    #
+    admin_session = _create_admin_session()
 
-        #
-        # Validate the state of the database
-        #
+    #
+    # Validate the state of the database
+    #
 
-        # Ensure the user_name is valid
-        if not _validate_user(user=user_name):
-            return (
-                "This user name is not allowed. Please make sure your user name doesn not start with 'user_' and is less than 32 characters long.",
-                400,
-            )
-
-        # ID of the user document and its content
-        user_doc_id = "org.couchdb.user:{}".format(user_name)
-        user_doc = {
-            "type": "user",
-            "name": user_name,
-            "password": user_password,
-            "roles": [],
-        }
-
-        # Check whether the user already exists (e.g., from a previous initialize)
-        response = admin_session.get(
-            urljoin(couchdb_client_config.baseurl, "_users/{}".format(user_doc_id)),
+    # Ensure the user_name is valid
+    if not _validate_user(user=user_name):
+        return (
+            "This user name is not allowed. Please make sure your user name doesn not start with 'user_' and is less than 32 characters long.",
+            400,
         )
-        if response.status_code != 200:
-            # The user does not already exist. Create the user.
-            response = admin_session.put(
-                urljoin(couchdb_client_config.baseurl, "_users/{}".format(user_doc_id)),
-                json=user_doc,
-            )
-            response.raise_for_status()
-        else:
 
-            return "The user already exists", 400
+    # ID of the user document and its content
+    user_doc_id = "org.couchdb.user:{}".format(user_name)
+    user_doc = {
+        "type": "user",
+        "name": user_name,
+        "password": user_password,
+        "roles": [],
+    }
 
-            # The user already exists, provide an 'If-Match' header with the revision.
-            # This will result in us overwriting the current document (e.g., a potential password change).
-            """
-            existing_user_doc = response.json()
-            response = admin_session.put(
-                urljoin(
-                    couchdb_client_config.baseurl,
-                    '_users/{}'.format(user_doc_id)
-                ),
-                headers={
-                    'If-Match': existing_user_doc['_rev']
-                },
-                json=user_doc,
-            )
-            response.raise_for_status()
-            """
-
-        # Check whether the database already exists (e.g., from a previous initialize).
-        database = _database_for_user(user=user_name)
-        response = admin_session.head(
-            urljoin(couchdb_client_config.baseurl, _database_for_user(user=user_name)),
-        )
-        if response.status_code != 200:
-            # The database does not already exist. Create the database.
-            response = admin_session.put(
-                urljoin(couchdb_client_config.baseurl, database),
-            )
-            response.raise_for_status()
-        else:
-            # In production, the database already existing is something we should have checked before starting.
-            # It would mean the account already exists, or that _database_for_user has generated a collision.
-            pass
-
-        # Ensure the database has the desired _security document.
+    # Check whether the user already exists (e.g., from a previous initialize)
+    response = admin_session.get(
+        urljoin(couchdb_client_config.baseurl, "_users/{}".format(user_doc_id)),
+    )
+    if response.status_code != 200:
+        # The user does not already exist. Create the user.
         response = admin_session.put(
-            urljoin(couchdb_client_config.baseurl, "{}/_security".format(database)),
-            json={
-                "members": {
-                    "names": [
-                        user_name,
-                    ],
-                    "roles": [
-                        "_admin",
-                    ],
-                },
-                "admins": {
-                    "roles": [
-                        "_admin",
-                    ],
-                },
-            },
+            urljoin(couchdb_client_config.baseurl, "_users/{}".format(user_doc_id)),
+            json=user_doc,
         )
         response.raise_for_status()
-        return {"user_name": user_name, "database": database}
+    else:
 
-        #
-        # Update the database
-        #
+        return "The user already exists", 400
+
+        # The user already exists, provide an 'If-Match' header with the revision.
+        # This will result in us overwriting the current document (e.g., a potential password change).
+        """
+        existing_user_doc = response.json()
+        response = admin_session.put(
+            urljoin(
+                couchdb_client_config.baseurl,
+                '_users/{}'.format(user_doc_id)
+            ),
+            headers={
+                'If-Match': existing_user_doc['_rev']
+            },
+            json=user_doc,
+        )
+        response.raise_for_status()
+        """
+
+    # Check whether the database already exists (e.g., from a previous initialize).
+    database = _database_for_user(user=user_name)
+    response = admin_session.head(
+        urljoin(couchdb_client_config.baseurl, _database_for_user(user=user_name)),
+    )
+    if response.status_code != 200:
+        # The database does not already exist. Create the database.
+        response = admin_session.put(
+            urljoin(couchdb_client_config.baseurl, database),
+        )
+        response.raise_for_status()
+    else:
+        # In production, the database already existing is something we should have checked before starting.
+        # It would mean the account already exists, or that _database_for_user has generated a collision.
+        pass
+
+    # Ensure the database has the desired _security document.
+    response = admin_session.put(
+        urljoin(couchdb_client_config.baseurl, "{}/_security".format(database)),
+        json={
+            "members": {
+                "names": [
+                    user_name,
+                ],
+                "roles": [
+                    "_admin",
+                ],
+            },
+            "admins": {
+                "roles": [
+                    "_admin",
+                ],
+            },
+        },
+    )
+    response.raise_for_status()
+    return {"user_name": user_name, "database": database}
+
+    #
+    # Update the database
+    #
 
 
 @users_blueprint.route("/get_profile", methods=["POST"])
