f55c6aad2244fe0332f3beb6ff383b1ccaf045cd
Anant Mittal <a42@Anants-MacBook-Pro.local>
2021-11-10 11:10:12 -0800

    Renamed the client secret key to something more generic

--- server_flask/config/dev.py
+++ server_flask/config/dev.py
@@ -2,14 +2,14 @@ import ruamel.yaml
 
 
 # Path is relative to server_flask
-COUCHDB_CLIENT_CONFIG_PATH = '../secrets/client/couchdb_client_config.yaml'
+COUCHDB_CLIENT_CONFIG_PATH = "../secrets/client/couchdb_client_config.yaml"
 
 
 class Config:
     # Expected by Flask
-    SECRET_KEY: str = 'SECRET_KEY'
+    SECRET_KEY: str = "SECRET_KEY"
 
-    ACCOUNT_CREATION_SECRET: str = 'ACCOUNT_CREATION_SECRET'
+    CLIENT_SECRET_KEY: str = "CLIENT_SECRET"
 
     URI_DATABASE: str
     DB_USER: str
@@ -19,6 +19,6 @@ class Config:
         with open(COUCHDB_CLIENT_CONFIG_PATH) as file_couchdb_client_config:
             couchdb_client_config = ruamel.yaml.safe_load(file_couchdb_client_config)
 
-        self.URI_DATABASE = couchdb_client_config['baseurl']
-        self.DB_USER = couchdb_client_config['admin']['user']
-        self.DB_PASSWORD = couchdb_client_config['admin']['password']
+        self.URI_DATABASE = couchdb_client_config["baseurl"]
+        self.DB_USER = couchdb_client_config["admin"]["user"]
+        self.DB_PASSWORD = couchdb_client_config["admin"]["password"]
