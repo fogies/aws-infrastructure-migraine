c7a44a7c2c81ee98c8a2085c6ee400d0ca614f86
James Fogarty <jayfo@users.noreply.github.com>
2021-11-13 06:13:39 -0800

    Remove Old Code

--- tasks/flask.py
+++ tasks/flask.py
@@ -25,29 +25,20 @@ def dev_serve(context):
     """
 
     if spawn_new_terminal(context):
-        # ssh_config = aws_infrastructure.tasks.ssh.SSHConfig.load(SSH_CONFIG_PATH)
-        # documentdb_config = aws_infrastructure.tasks.library.documentdb.DocumentDBConfig.load(DOCUMENTDB_CONFIG_PATH)
-        #
-        # with aws_infrastructure.tasks.ssh.SSHClientContextManager(ssh_config=ssh_config) as ssh_client:
-        #     with aws_infrastructure.tasks.ssh.SSHPortForwardContextManager(
-        #         ssh_client=ssh_client,
-        #         remote_host=documentdb_config.endpoint,
-        #         remote_port=documentdb_config.port,
-        #     ):
-                with context.cd(Path(FLASK_DIR)):
-                    context.run(
-                        command=' '.join([
-                            'pipenv',
-                            'run',
-                            'flask',
-                            'run',
-                        ]),
-                        env={
-                            'FLASK_ENV': 'development',
-                            'FLASK_RUN_HOST': 'localhost',
-                            'FLASK_RUN_PORT': '4000',
-                        },
-                    )
+        with context.cd(Path(FLASK_DIR)):
+            context.run(
+                command=' '.join([
+                    'pipenv',
+                    'run',
+                    'flask',
+                    'run',
+                ]),
+                env={
+                    'FLASK_ENV': 'development',
+                    'FLASK_RUN_HOST': 'localhost',
+                    'FLASK_RUN_PORT': '4000',
+                },
+            )
 
 
 @task
