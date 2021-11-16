d19b2033f806f7a141fea26a66c25ef3f083489f
James Fogarty <jayfo@users.noreply.github.com>
2021-11-16 09:50:04 -0800

    Flask Docker and Helm

--- terraform/terraform_ecr/terraform.tf
+++ terraform/terraform_ecr/terraform.tf
@@ -14,6 +14,6 @@ module "ecr" {
   source = "github.com/fogies/aws-infrastructure//terraform_common/ecr"
 
   names = [
-    "aws_infrastructure_migraine/server_flask",
+    "aws_infrastructure_migraine/migraine_flask",
   ]
 }
